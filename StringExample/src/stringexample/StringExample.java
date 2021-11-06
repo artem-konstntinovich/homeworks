252e5aa1f8369d44b52085ef91f964a71b0915d0
Artem K. Koltunov <artem.chistyakov@gmail.com>
2021-11-06 10:12:56 +0200

    add examples
    add examples

--- StringExample/src/stringexample/StringExample.java
+++ StringExample/src/stringexample/StringExample.java
@@ -8,10 +8,47 @@ public class StringExample {
         
         System.out.println("Как вас зовут?");
         Scanner input = new Scanner(System.in);
-        //String name = input.nextLine();
-        String name = input.next();
+        String name = input.nextLine();
+        //String name = input.next();// input.nextLine();
+        // String name = input.next("");// pattern ДЗ найти примеры паттернов строк для next
+        //String StringBuilder StringBuffer
         System.out.println("Ну, привет, " + name);
+        //Длина строки
+        int n = name.length();
+        int numLastChar = n - 1;// номер последней буквы
+        //Чтение заданного символа-буквы (нумерация от нуля)
+        for(int i = numLastChar; i >= 0; i--)
+        {
+            System.out.println(name.charAt(i));//charAt(i) возвращает символ под номером i
+        }
+        //получение части строки, например, все символы между вторым и четвертым (включительно)
+        if (n >= 4)
+        {
+            //Ivan
+            //0123
+            String newString = name.substring(1,3 + 1);
+            //0 - первый
+            //1 - второй
+            //3 - четвертый
+            System.out.println(newString);
+        }
+        //разбиение строки на части, например, все слова отдельно
+        //Получит отдельно Имя и Фамилию
+        String[] words = name.split(" ");
+        int M = words.length;// M - будет количеством слов
+        String firstName = words[0];
+        String lastName = words[1];
+        System.out.printf("Вы ввели слов %d, это имя = %s и фамилия = %s \n", M, firstName, lastName);
         
+        //определение, входит ли одна строка в другую
+        boolean result = name.contains(lastName);
+        if (result == true)
+        {
+            System.out.println("Строка = "+name+" содержит строку = "+lastName);
+        }
+        
+        
+          
         
         
     }
