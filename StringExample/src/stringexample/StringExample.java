f026cf9f0621a71c54cd2b8a3139f3e72de9d443
Artem K. Koltunov <artem.chistyakov@gmail.com>
2021-11-06 09:36:03 +0200

    String example 1
    example Hello

---
+++ StringExample/src/stringexample/StringExample.java
@@ -0,0 +1,19 @@
+package stringexample;
+import java.util.Scanner;
+
+public class StringExample {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Как вас зовут?");
+        Scanner input = new Scanner(System.in);
+        //String name = input.nextLine();
+        String name = input.next();
+        System.out.println("Ну, привет, " + name);
+        
+        
+        
+    }
+    
+}
